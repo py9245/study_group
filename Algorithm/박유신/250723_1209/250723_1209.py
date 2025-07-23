98a0bccfa151c67f8c784263ec21d165b6432766
Yusin <py9245@naver.com>
2025-07-23 11:12:07 +0900

    첫번째문제

---
+++ Algorithm/박유신/250723_1209/250723_1209.py
@@ -0,0 +1,15 @@
+for _ in range(1, 11):
+    t = int(input())
+    row = [0 for _ in range(100)]
+    col = [0 for _ in range(100)]
+    bsl = 0
+    sl = 0
+    for i in range(100):
+        for j, n in enumerate(map(int, input().split())):
+            row[i] += n
+            col[j] += n
+            if i == j :
+                bsl += n
+            if i + j == 99:
+                sl += n
+    print(f"#{t} {max(max(col),max(row),bsl,sl)}")
