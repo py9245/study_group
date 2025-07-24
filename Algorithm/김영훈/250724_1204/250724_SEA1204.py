047a790190a9836a188bd234ef0e8361482b3536
김영훈 <yhkor11@naver.com>
2025-07-25 01:00:41 +0900

    최빈수 수정

--- Algorithm/김영훈/250724_1204/250724_SEA1204.py
+++ Algorithm/김영훈/250724_1204/250724_SEA1204.py
@@ -9,6 +9,34 @@ for t in range(1, T+1):
         Max = max(Max, B)
     print(f"#{t}", Max)
 
-# 최빈수의 횟수를 구해서 최빈수를 출력해보려 합니다.(1번 결과 17 -> 17번 나온 숫자 찾기)
-# #1 17 결과 나온 후 N = list(map(int, '''input()'''.split())) -> EOFError: EOF when reading a line
-# 소요 시간 = 50분
+'''
+최빈수의 횟수를 구해서 최빈수를 출력해보려 합니다.(1번 결과 17 -> 17번 나온 숫자 찾기)
+#1 17 결과 나온 후 N = list(map(int, input().split())) -> EOFError: EOF when reading a line
+                                   ~^^^^^
+소요 시간 = 50분
+----------------------------------------------------------
+T = int(input())
+A = int(input())
+Max = 0
+t= 1
+for t in range(1, T+1):
+    N = list(map(int, input().split()))
+    for n in range(1, 101):
+        B = N.count(n)
+        Max = max(Max, B)
+        for d in range(0, len(N)):
+            if(N[d]<Max):
+                # print(N[d])
+                del N[d]
+            
+    print(f"#{t}", N[0])
+----------------------------------------------------------
+이 코드 41 부터 시작해야되는데
+11
+13
+12 이렇게 나옴
+소요시간 약 1시간 
+if(N[d]<Max):
+       ~^^^
+IndexError: list index out of range
+'''
