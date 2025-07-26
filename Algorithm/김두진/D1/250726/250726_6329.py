a46e18af0d4af1d4f9dad1efe086d84bfac88ee1
DESKTOP-BTDJ0S7\Administrator <enwls5626@gmail.com>
2025-07-26 23:07:44 +0900

    t

--- Algorithm/김두진/D1/250726/250726_6329.py
+++ Algorithm/김두진/D1/250726/250726_6329.py
@@ -2,7 +2,7 @@ def countdown(n):
     if n <= 0 :
         print("카운트다운을 하려면 0보다 큰 입력이 필요합니다.")
     if n > 0 :
-        for i in range(n, 0, -1):
+        for i in range(n, 0, -1): # n : 시작값, 0 : 끝값, -1 : 감소값
             print(i)
     
 countdown(0)
