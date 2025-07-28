0115ab0ebd33afb3393ff5fc44d212d59ae0426e
py9245 <py9245@naver.com>
2025-07-28 12:33:16 +0900

    python_first_study

---
+++ SSAFY/박유신/python/2507/250728.py
@@ -0,0 +1,11 @@
+# print("hello world")
+# print(-2 ** 4)
+# number = 10
+# double = number * 2
+# print(double)
+
+# number = 5
+# double = number * 2
+# print(double)
+a = "abcde"
+print(a[:5:-1]) # False
