6b00eeab651dcc632a1277f764055c90e22f3775
김영훈 <yhkor11@naver.com>
2025-08-02 23:44:54 +0900

    file upload

--- Algorithm/김영훈/baekjoon/250829_1333/250729_baek1333.py
+++ Algorithm/김영훈/baekjoon/250829_1333/250729_baek1333.py
@@ -1,26 +1,25 @@
-N, L, D = map(int, input().split()) #곡 수
-# L = int(input()) #노래 길이
-# D = int(input()) #전화벨 울리는 주기
-sum = L+5
-A=D
+N, L, D = map(int, input().split())
+# N = 곡 수
+# L = 노래 길이
+# D = 전화벨 울리는 주기
+
+sum = L+5                       # 노래 끝나고부터 전화받을 수 있는 시간 범위
+A=D                             #  전화벨 울리는 주기
+B= L                            # 노래 끝나는 시간 구하기 위해 노래 길이
+cnt = 0                         # 
 for n in range(1, N+1):
-    
     if(L<=A<L+5):
-        print(n, "if")
+        # print(n, "if")
         break
-    elif (L+5<A):
+    elif (L+5<=A<(N*B)+(N*5)):
         L += sum
-        print(n, "elif")
-        
+        # print(n, "elif")
     while(A<L):
         A+=D
-        print(A, n, "while")
-    print(n)
+        # print(A, n, "while")
+        
+    # print(n,"n")
 print(A)
-    # if(D<L and n == N):
-    #     D=D*2
 '''
-새로운 반례가 계속 생기는 거 같아서 아직 푸는 중입니다.
-
-더 풀기 전에 오늘 실습 진행완료하고 이어서 하려합니다.
+통과
 '''
