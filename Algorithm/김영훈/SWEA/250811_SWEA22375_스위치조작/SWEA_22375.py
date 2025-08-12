6a69f4e542b5470bed2d9e4a6fbcca9f55aad9e5
김영훈 <yhkor11@naver.com>
2025-08-12 11:38:33 +0900

    modify file 스위치조작

--- Algorithm/김영훈/SWEA/250811_SWEA22375_스위치조작/SWEA_22375.py
+++ Algorithm/김영훈/SWEA/250811_SWEA22375_스위치조작/SWEA_22375.py
@@ -7,17 +7,13 @@ for test in range(1, Test+1):
     cnt = 0
     for idx, a in enumerate(after):
         if a != before[idx]:
-
-            for jdx in range(num):
-                if idx+jdx < num:  # 전구 범위 안넘게
-                    if after[idx+jdx] == 1:  # after 의 인덱스가 1이면
-                        for kdx in range(after[idx+jdx], after[-1]):
-                            after[kdx] = 0
-                            cnt += 1
-                    elif after[idx+jdx] == 0:
-                        for kdx in range(after[idx+jdx], after[-1]):
-                            after[kdx] = 1
-                            cnt += 1
+            cnt += 1
+            for jdx, b in enumerate(before):
+                if idx+jdx < num:  # 전구 다르고 전구 범위 안넘으면
+                    if after[idx+jdx] == '1':  # after 의 인덱스가 1이면
+                        after[idx+jdx] = '0'
+                    elif after[idx+jdx] == '0':
+                        after[idx + jdx] = '1'
         elif a == before[idx]:
             pass
-    print(cnt, after, before)
+    print(f"#{test} {cnt}")
