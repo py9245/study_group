b4eea299c07710bb2c8d8d18df16cb07f5db3802
DESKTOP-BTDJ0S7\Administrator <enwls5626@gmail.com>
2025-08-18 23:48:10 +0900

    0818_오셀로_실패

--- Algorithm/김두진/baekjoon/250818/250818_bj10163.py
+++ Algorithm/김두진/baekjoon/250818/250818_bj10163.py
@@ -1,3 +1,7 @@
 # 백준 색종이 10163번 문제
 N = int(input()) # 색종이 장수
-a, b, c, d = map(int,input().split()) # a.b는 가장 왼쪽 아래, c,d는 너비와 높이
+arr = [[0] * 1001 for _ in range(1001)] # 색종이가 놓이는 평면은 가로 최대 1001칸,세로 최대 1001칸
+
+for i in range(1,N+1):
+    x, y, w, h = map(int,input().split()) # x,y는 가장 왼쪽 아래, w,h는 너비와 높이
+    
