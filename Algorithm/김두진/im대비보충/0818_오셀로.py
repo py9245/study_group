e8be2a01555486f3ceccd7eba40a008f4a0a9636
DESKTOP-BTDJ0S7\Administrator <enwls5626@gmail.com>
2025-08-18 22:02:05 +0900

    오셀론

--- Algorithm/김두진/im대비보충/0818_오셀로.py
+++ Algorithm/김두진/im대비보충/0818_오셀로.py
@@ -13,10 +13,9 @@ for test_case in range(1,t+1):
     arr[2][2] = 2
     arr[2][1] = 1
     arr[1][2] = 1
-    for i in range(M):
+    for a in range(M):
         # 입력 받아오기 이 때, c에 들어오는 1, 2 는 흑돌과 백돌을 의미함
         x, y, c = map(int,input().split())
         arr[x-1][y-1] = c
         
-    
     print(arr)
