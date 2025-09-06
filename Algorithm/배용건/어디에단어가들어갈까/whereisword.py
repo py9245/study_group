f16b094227a815cccb68b506a06309215f50d66b
배용건 <rjslsla12@naver.com>
2025-09-06 13:04:22 +0900

    where

---
+++ Algorithm/배용건/어디에단어가들어갈까/whereisword.py
@@ -0,0 +1,31 @@
+T = int(input())
+ 
+for tc in range(1, T + 1):
+    N, K = map(int, input().split())
+    puzzle = [list(map(int, input().split())) for _ in range(N)]
+    result  = 0
+ 
+    for i in range(N):
+        empty_cnt = 0
+        for j in range(N):
+            if puzzle[i][j] == 1:
+                empty_cnt += 1
+            elif puzzle[i][j] == 0:
+                if empty_cnt == K:
+                    result += 1
+                empty_cnt = 0
+        if empty_cnt == K:
+            result += 1
+     
+    for j in range(N):
+        empty_cnt = 0
+        for i in range(N):
+            if puzzle[i][j] == 1:
+                empty_cnt += 1
+            elif puzzle[i][j] == 0:
+                if empty_cnt == K:
+                    result += 1
+                empty_cnt = 0
+        if empty_cnt == K:
+            result += 1
+    print(f'#{tc} {result}')
