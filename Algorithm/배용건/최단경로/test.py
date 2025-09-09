98eeb34240864d5935e8d129170d2259849722e0
배용건 <rjslsla12@naver.com>
2025-09-09 18:00:00 +0900

    study_09

--- Algorithm/배용건/최단경로/test.py
+++ Algorithm/배용건/최단경로/test.py
@@ -2,14 +2,14 @@
 
 import sys
 import heapq
-sys.stdin = open('.txt','r')
+sys.stdin = open('sample_input (2).txt','r')
 
 
 
 
 def dijkstra(graph, start, goal):
     N = len(graph) - 1  # graph 마지막 정점 번호
-    dist = [float('inf')] * N + 1
+    dist = [float('inf')] * (N + 1)
     dist[start] = 0
     heap = [(0, start)]  # 현재까지의 거리, 정점
 
@@ -25,7 +25,7 @@ def dijkstra(graph, start, goal):
             # 정점이 끝까지 갔다면 최소 가중치를 return
             return cur_dist
         
-        for e, w in graph[n]:
+        for e, w in graph[n]:  # 현재정점에서 e까지 갈수있는 모든길을 순회
             # graph의 정점인덱스 값을 e, w 로 순회 시킨다
             new_dist = cur_dist + w
             # new_dist 에 최소 가중치 와 그래프 정점의 가중치를 더한다
