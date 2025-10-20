9573d49a5591b4371141248e323305d962a5a280
배용건 <rjslsla12@naver.com>
2025-10-20 23:15:46 +0900

    bj1931

---
+++ Algorithm/배용건/im역량평가문제/알고리즘모음/희의실/boj1931.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt','r')
+
+N = int(input())
+n_list = [list(map(int, input().split())) for _ in range(N)]
+re_list = sorted(n_list, key=lambda x: (x[1],x[0]))
+
+one_list = re_list[0]
+cnt = 1
+for a, b in re_list:
+    if one_list[1] <= a:
+        cnt += 1
+        one_list = [a, b]
+
+print(one_list)
+print(cnt)
