136be320b85d89e2b70d75360573ef1364244e5b
배용건 <rjslsla12@naver.com>
2025-10-25 23:51:37 +0900

    bj2605

---
+++ Algorithm/배용건/im역량평가문제/알고리즘모음/줄세우기/bj2605.py
@@ -0,0 +1,36 @@
+import sys
+sys.stdin = open('input.txt','r')
+# 첫번째로 줄을 선 학생은 무조건 0번을 받아 제일 앞에 줄을 선다
+# 첫번째 학생을 만든 리스트에서 디폴트로 먼저 넣고 시작하기
+
+# 두번째 줄을 선 학생은 
+
+# 0번을 뽑으면 그대로 (본인 인덱스 그대로)
+
+# 1번을 뽑으면 자기 인덱스 앞에 사람의 앞으로 감
+
+# 세번째 줄을 선 학생은 
+
+# 0번을 뽑으면 자기 인덱스 그대로
+
+# 뽑은 번호만큼 본인 인덱스에서 앞으로 간다
+
+
+N = int(input())
+line_num = list(map(int, input().split())) 
+student = []
+real_line = [0] * N
+for i in range(1, N + 1):
+    student.append(i)
+
+real_line[N-1] = student[0]
+
+for j in range(1, N):
+    if line_num[j] == 0:
+        real_line[j] = student[j]
+    if line_num[j] != 0:
+        
+    
+
+
+
